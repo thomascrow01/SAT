0addec98224e68916c1ac8c3958439cf86ec2589
Thomas Crow <thomascrow01@gmail.com>
2018-08-03 09:58:01 +1000

    Update login2.php

--- php/login2.php
+++ php/login2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <!-- login2.php Thomas Crow 7/31/2018 -->
-<!-- Last updated 7/31/2018 -->
+<!-- Last updated 8/3/2018 -->
 
 <head>
 	<title>SAT</title>
@@ -11,8 +11,8 @@
 	<?php
 		session_start();
 		include 'functions.php';
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+		$username1 = $_POST['username'];
+		$password1 = $_POST['password'];
 		
 		$servername = "localhost";
 		$username = "root";
@@ -24,25 +24,38 @@
 			die("Connection failed: " . $conn->connect_error);
 		}
 		//echo "Connected successfully";
-		$sql = "SELECT * FROM sat_database.users ORDER BY Id";
+		$sql = "SELECT * FROM sat_database.users ORDER BY userID";
 		$result = $conn->query($sql);
 		// A linear search will check to see if the username and password match any in the database.
 		while($row = $result->fetch_assoc()) {
-				if($row["username"] == $username and $row["password"] == $password){
+				if($row["username"] == $username1 and $row["password"] == $password1){
 					// if true, the session variable "loggedin" will be set to true.
 					$_SESSION["loggedin"] = true;
 					$_SESSION["username"] = $row["username"];
+					$_SESSION["name"] = $row["fullname"];
+					$_SESSION["adminstatus"] = $row["adminstatus"];
 				}
 			}
 		$conn->close();
 	?>
 	<div class="menu">
 		<table>
-			<tr onclick="location.href='php/login.php';" style="cursor:pointer;">
-				<td>
-					<h3>Login</h3>
-				</td>
-			</tr>
+			<?php
+				/* Check if user is logged in. */
+				if($_SESSION['loggedin'] == true){
+					?><tr onclick="location.href='logout.php';" style="cursor:pointer;">
+						<td>
+							<h3>Logout</h3>
+						</td>
+					</tr><?php
+				}else{
+					?><tr onclick="location.href='login1.php';" style="cursor:pointer;">
+						<td>
+							<h3>Login</h3>
+						</td>
+					</tr><?php
+				}
+			?>
 			<tr onclick="location.href='php/calendar.php';" style="cursor:pointer;">
 				<td>
 					<h3>Calendar</h3>
@@ -71,3 +84,4 @@
 		?>
 	</div>
 </body>
+
